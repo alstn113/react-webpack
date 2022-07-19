870a59202011895ac0c7a8ea0e193b4cf384a864
neko113 <alstn113@gmail.com>
2022-07-20 08:57:55 +0900

    chore : pre_push 폴더 이름 변경

--- src/components/ErrorFallback/ErrorFallback.tsx
+++ src/components/ErrorFallback/ErrorFallback.tsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
-import { useQueryClient } from "@tanstack/react-query";
+import { useEffect } from 'react';
+import { useQueryClient } from '@tanstack/react-query';
 
 interface Props {
   message: string;
